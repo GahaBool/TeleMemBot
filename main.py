03eae7f9112b042b240c9a61174940b21dbbc3db
Egor Tirskih_laptop <egortirskih1984@gmail.com>
2024-05-22 16:57:02 +0800

    Сделал каркас будущего бота!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from telethon import TelegramClient, events
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
 from mysql.connector import connect, Error, errorcode
-from MySQL_Query import create_table, add_new_mem
 from dotenv import dotenv_values
 
 #Создал файл специально, что бы можно было работать с личными данными.
@@ -8,54 +10,34 @@ from dotenv import dotenv_values
 #================================================================
 config = dotenv_values()
 
-#Подключение к базам MySQL
+#Бот aiogram
 #================================================================
-def connection_db():
+chat_id = 1002001934089
+bot = Bot(config["TOKEN"])
+dp = Dispatcher()
+
+async def send_photo():
     try:
         connection = connect(host=config["HOST"],
                              user=config["USER"],
                              password=config["PASSWORD"],
-                             db=config["DATABASES"],)
-        print("successfully connected...")
-        print("################################################################")
-    except Error as err:
-        print(f"The error '{err}' occurred")
-    return connection
+                             db=config["DATABASES"], )
+        cursor = connection.cursor()
 
-connection = connection_db()
+        # Получаем изображение из базы данных
+        cursor.execute("SELECT imag FROM membot WHERE id = %s", (1,))
+        image = cursor.fetchone()[0]
 
-#Позволяет при получении новых сообщений сохранять карнтинки
-#================================================================
-#данные для подключения к API client
-bot = 'MemBot'
-api_id = config["API_ID"]
-api_hash = config["API_HASH"]
+        # Отправляем изображение в чат
+        await bot.send_photo(chat_id=chat_id, photo=image)
 
-client = TelegramClient(bot, api_id, api_hash)
+    except Error as e:
+        print(f"Ошибка при работе с MySQL: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 
-channel_id = 1002096895063
-# Обработчик новых сообщений
-@client.on(events.NewMessage(chats=channel_id))
-async def handler(event):
-    # Извлекаем текст сообщения
-    message_text = event.message.message or "No text"
-
-    # Проверяем, есть ли у сообщения медиа в виде фото
-    if event.message.photo:
-        # Скачиваем медиа-файл асинхронно в файл
-        img = await event.message.download_media(file=bytes)
-        # Получаем имя файла для сохранения
-        img_name = f"{event.message.photo.id}.jpg"
-
-        create_table(connection)#Создание таблицы MeSQL или проверка была ли она создана ранее
-        add_new_mem(connection, img, img_name, message_text)#Добавление новой записи в MySQL
-
-        print("Был опублекован новый пост!")#Стандартное термининальное оповищение.
-
-# Запуск клиента
-client.start(password=config["PASSOWORD_TG"])#Необходим пароль если у вас влюченеа двухфакторная аутентификация телеграм
-client.run_until_disconnected()#Позволяет циклу идти бесконечно
-
-
-
-
+# Запуск бота
+if __name__ == '__main__':
+    asyncio.run(send_photo())
